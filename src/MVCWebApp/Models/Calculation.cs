7727aa6e381779ed1701360ad79abf7c627dfd37
Scott Lusk <slusk@hitachisolutions.com>
2021-11-15 23:09:54 -0500

    add simple calculator

---
+++ src/MVCWebApp/Models/Calculation.cs
@@ -0,0 +1,18 @@
+﻿namespace Accelerantx.MVCWebApp.Models
+{
+    public class Calculation
+    {
+        public int Input1 { get; set; }
+        public int Input2 { get; set; }
+        public Operation Operator { get; set; }
+        public int? Result { get; set; }
+    }
+
+    public enum Operation
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+}
